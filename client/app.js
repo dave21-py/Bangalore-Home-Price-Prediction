3d35147b4e0879813659bddf8d409fe57c9ee5da
David Geddam <dgedd236@students.bju.edu>
2025-10-16 05:06:26 -0400

    changed localhost to function calling

--- client/app.js
+++ client/app.js
@@ -37,7 +37,7 @@ function onClickedEstimatePrice() {
 
   // --- CORRECTED THIS SECTION ---
 
-  var url = "http://127.0.0.1:5000/predict_home_price"; // Use this if you are NOT using nginx which is first 7 tutorials
+  var url = "/predict_home_price"; // Use this if you are NOT using nginx which is first 7 tutorials
 
   // var url = "/api/predict_home_price"; // Use this if you are using nginx. i.e tutorial 8 and onwards
 
@@ -70,7 +70,7 @@ function onPageLoad() {
 
   // --- AND CORRECTED THIS SECTION ---
 
-  var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+  var url = "/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
 
   // var url = "/api/get_location_names"; // Use this if you are using nginx. i.e tutorial 8 and onwards
 
